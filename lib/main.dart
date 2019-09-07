91f48fa281e6423b4aafbaa459dd931cef29d0c3
Krunk-Juice <krunk_jc@yahoo.com>
2019-09-07 16:48:36 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'login_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, // this removes the debug banner
+      title: 'Parking App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
---
+++ md
@@ -0,0 +1,1 @@
+"#CSUF-Parking-App" 
