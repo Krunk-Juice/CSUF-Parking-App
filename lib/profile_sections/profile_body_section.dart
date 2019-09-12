4d358ae24034d33aa14b48047ce1607d51a99e64
Krunk-Juice <krunk_jc@yahoo.com>
2019-09-11 21:25:53 -0700

    Big Commit

--- lib/profile_sections/profile_body_section.dart
+++ lib/profile_sections/profile_body_section.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_parking_app/contacts_page.dart';
 
 class ProfilePageBody extends StatelessWidget {
   ProfilePageBody(this.profile);
