066536b644709b32fb391cc4c63d72c6bcd038ba
Krunk-Juice <krunk_jc@yahoo.com>
2019-09-13 05:05:50 -0700

    minor

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/alex/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/alex/AndroidStudioProjects/CSUF-Parking-App"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_ROOT=D:\Users\krunk\AppData\Local\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\Christopher Ta\Cal State Fullerton\Fall 2019\CPSC 362\Project\flutter_parking_app"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/home/alex/development/flutter/bin/cache/artifacts/engine/ios"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=D:\Users\krunk\AppData\Local\flutter\bin\cache\artifacts\engine\ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
