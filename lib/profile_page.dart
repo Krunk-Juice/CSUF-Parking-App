3a7a4d1608e237b0787d6fd27bcac087521992df
alex <huyho114@gmail.com>
2019-09-13 20:48:20 -0700

    update profile with sections

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -28,88 +28,8 @@ class MapScreenState extends State<ProfilePage> with SingleTickerProviderStateMi
             Column(children: <Widget>[
 
               /* Header and Image Section */
-<<<<<<< HEAD
               ProfileHeaderSection(),
               ProfileBodySection(),
-=======
-              Container(
-                height: 250,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: [Colors.lightBlue, Colors.cyan],
-                    begin: Alignment.topLeft,
-                    end: FractionalOffset(0.2, 0.8)
-                  )
-                ),
-                child: Column(children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(left: 20, top: 20),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        /* < PROFILE */
-                        Icon(
-                          Icons.arrow_back_ios,
-                          color: Colors.white,
-                          size: 22,
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(left: 25),
-                          child: Text('PROFILE',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                              fontFamily: 'sans-serif-light',
-                              color: Colors.white
-                            )),)
-                      ],)
-                  ),
-
-                  /* Profile Image and Camera Icon */
-                  Padding(
-                    padding: EdgeInsets.only(top: 20),
-                    child: Stack(fit: StackFit.loose, children: <Widget>[
-                      Row(crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        /* Profile Image */
-                        Container(
-                          width: 140,
-                          height: 140,
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            border: Border.all(
-                              width: 15,
-                              color: Colors.blue),
-                            image: DecorationImage(
-                              image: ExactAssetImage(
-                                'assets/images/undraw_profile_pic_ic5t.png'
-                              ),
-                              fit: BoxFit.cover,),
-                          )),
-                      ],),
-                      Padding(
-                        padding: EdgeInsets.only(top: 90, right: 100),
-                        child: Row(
-                          mainAxisAlignment:  MainAxisAlignment.center,
-                          children: <Widget>[
-                            /* Camera Icon */
-                            CircleAvatar(
-                              backgroundColor: Colors.red,
-                              radius: 25,
-                              child: Icon(
-                                Icons.camera_alt,
-                                color: Colors.white,
-                              ),
-                            )
-                          ],
-                        )),
-                    ]),
-                  )
-                ],
-                ),),
-                
->>>>>>> 9828211169dd0c283d3d19c03ff0ea1a2ed9e42a
                 /* Infomation Section */
             ],)
           ],
