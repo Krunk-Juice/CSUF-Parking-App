a5903a1cdbd4cfe86400e7dafd35dfd7856d0c43
alex <huyho114@gmail.com>
2019-09-13 23:43:14 -0700

    update route back to login page

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      initialRoute: 'profile_page',
+      initialRoute: 'login_page',
       routes: {
         'login_page': (context)=>LoginPage(),
         'home_page': (context)=>HomePage(),
