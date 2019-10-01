87a1fb236bf593d53d431069c2c97007358d7000
Krunk-Juice <krunk_jc@yahoo.com>
2019-10-01 16:17:17 -0700

    added password confimation check

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/alex/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/alex/AndroidStudioProjects/CSUF-Parking-App"
+export "FLUTTER_ROOT=/home/Chris/Development/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/Chris/Documents/CPSC 362/CSUF-Parking-App"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/home/alex/development/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/home/Chris/Development/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
--- lib/screens/register/register_body.dart
+++ lib/screens/register/register_body.dart
@@ -43,7 +43,7 @@ class _RegisterBodyState extends State<RegisterBody> {
     // Force refresh input
     setState(() {});
   }
-void handleUpdateData() {
+  void handleUpdateData() {
     // focusNodeNickname.unfocus();
     // focusNodeAboutMe.unfocus();
 
