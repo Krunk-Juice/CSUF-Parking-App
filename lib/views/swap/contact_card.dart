37bd29e06f2df6995a1123f1c4bab79ad3e7deaa
hho114 <hho114@csu.fullerton.edu>
2019-11-05 18:55:45 -0800

    update swap location

--- lib/views/swap/contact_card.dart
+++ lib/views/swap/contact_card.dart
@@ -11,27 +11,23 @@ SharedPreferences prefs;
 
 // Confirmation Screen
 class ContactCard extends StatefulWidget {
-
-ContactCard({this.swaperId,this.swaperName,this.swaperPhotoUrl});
-
+  ContactCard(
+      {this.swaperId, this.swaperName, this.swaperPhotoUrl, this.swapLocation});
 
   final String swaperId;
   final String swaperName;
   final String swaperPhotoUrl;
-  
-  
+  final String swapLocation;
+
   @override
   _ContactCardState createState() => _ContactCardState();
 }
 
 class _ContactCardState extends State<ContactCard> {
-  
-
   String id = '';
-  
   Future<void> _launched;
   String swaperPhoneNumber = '';
-  
+  String urlLocation = '';
 
   // Initialize State Confirmation Screen
   @override
@@ -39,13 +35,13 @@ class _ContactCardState extends State<ContactCard> {
     super.initState();
 
     readLocal();
+    getUrlLocation();
   }
 
   // Read RAM for user information.
   void readLocal() async {
     prefs = await SharedPreferences.getInstance();
     id = prefs.getString('id') ?? '';
-    
 
     Firestore.instance
         .collection('users')
@@ -53,7 +49,6 @@ class _ContactCardState extends State<ContactCard> {
         .get()
         .then((DocumentSnapshot snapshot) {
       swaperPhoneNumber = snapshot.data['phone'].toString();
-      
     });
 
     // Force refresh input
@@ -64,99 +59,107 @@ class _ContactCardState extends State<ContactCard> {
   @override
   Widget build(BuildContext context) {
     return Container(
-            //color: Colors.blueGrey,
-            child: Column(
+        //color: Colors.blueGrey,
+        child: Column(
+      children: <Widget>[
+        Column(
           children: <Widget>[
-            Column(
-              children: <Widget>[
-                /* Header Section */
-                Container(
-                    height: 350,
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                          colors: [Colors.lightBlue, Colors.cyan],
-                          begin: Alignment.topLeft,
-                          end: Alignment(-0.2, 0.7)),
-                    ),
-                    child: Column(
-                      children: <Widget>[
-                        Padding(
-                            padding: EdgeInsets.only(top: 30),
-                            // child: Stack(fit: StackFit.loose, children: <Widget>[
-                            //   /* Container fills the full width of the device */
-                            child: Row(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Material(
-                                    color: Colors.blue,
-                                    borderRadius: BorderRadius.circular(24.0),
-                                    child: ClipOval(
-                                      child: widget.swaperPhotoUrl == null
-                                          ? Icon(
-                                              Icons.account_circle,
-                                              size: 90.0,
-                                              color: Colors.grey,
-                                            )
-                                          : CachedNetworkImage(
-                                              imageUrl: widget.swaperPhotoUrl,
-                                              width: 50.0,
-                                              height: 50.0,
-                                            ),
-                                    ))
-                              ],
-                            )
-                            //],)
-                            ),
-                        Padding(
-                            padding: EdgeInsets.only(top: 30),
-                            child: Row(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Text(widget.swaperName,
-                                    style: TextStyle(
-                                        fontSize: 30,
-                                        fontWeight: FontWeight.normal,
-                                        color: Colors.white)),
-                              ],
-                            ))
-                      ],
-                    )),
-                SizedBox(
-                  height: 50,
+            /* Header Section */
+            Container(
+                height: 350,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      colors: [Colors.lightBlue, Colors.cyan],
+                      begin: Alignment.topLeft,
+                      end: Alignment(-0.2, 0.7)),
                 ),
-                /* Button Container */
-                Row(
+                child: Column(
                   children: <Widget>[
-                    RoundedButton(
-                      colour: Colors.blueAccent,
-                      title: 'Call',
-                      onPressed: () => _handleCall(),
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    RoundedButton(
-                      colour: Colors.greenAccent,
-                      title: 'Message',
-                      onPressed: () => _handleMessage(),
-                    ),
+                    Padding(
+                        padding: EdgeInsets.only(top: 30),
+                        // child: Stack(fit: StackFit.loose, children: <Widget>[
+                        //   /* Container fills the full width of the device */
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Material(
+                                color: Colors.blue,
+                                borderRadius: BorderRadius.circular(24.0),
+                                child: ClipOval(
+                                  child: widget.swaperPhotoUrl == null
+                                      ? Icon(
+                                          Icons.account_circle,
+                                          size: 90.0,
+                                          color: Colors.grey,
+                                        )
+                                      : CachedNetworkImage(
+                                          imageUrl: widget.swaperPhotoUrl,
+                                          width: 50.0,
+                                          height: 50.0,
+                                        ),
+                                ))
+                          ],
+                        )
+                        //],)
+                        ),
+                    Padding(
+                        padding: EdgeInsets.only(top: 30),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text(widget.swaperName,
+                                style: TextStyle(
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.normal,
+                                    color: Colors.white)),
+                          ],
+                        ))
                   ],
+                )),
+            SizedBox(
+              height: 20,
+            ),
+            RoundedButton(
+              colour: Colors.orangeAccent,
+              title: 'Navigation',
+              onPressed: () => _handleNavigation(),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            /* Button Container */
+            Row(
+              children: <Widget>[
+                RoundedButton(
+                  colour: Colors.blueAccent,
+                  title: 'Call',
+                  onPressed: () => _handleCall(),
                 ),
                 SizedBox(
-                  height: 10,
+                  width: 10,
                 ),
                 RoundedButton(
-                  colour: Colors.redAccent,
-                  title: 'Finish',
-                  onPressed: () => _handleFinish(context),
-                )
+                  colour: Colors.greenAccent,
+                  title: 'Message',
+                  onPressed: () => _handleMessage(),
+                ),
               ],
             ),
-            FutureBuilder<void>(future: _launched, builder: _launchStatus),
+            SizedBox(
+              height: 10,
+            ),
+            RoundedButton(
+              colour: Colors.redAccent,
+              title: 'Finish',
+              onPressed: () => _handleFinish(context),
+            )
           ],
-        ));
+        ),
+        FutureBuilder<void>(future: _launched, builder: _launchStatus),
+      ],
+    ));
   }
 
   // You complete the swap and exit the screen
@@ -169,7 +172,6 @@ class _ContactCardState extends State<ContactCard> {
         actions: <Widget>[
           new FlatButton(
             onPressed: () {
-
               Firestore.instance.collection('users').document(id).updateData({
                 'status': 'Relaxing',
               }).then((data) async {
@@ -178,7 +180,6 @@ class _ContactCardState extends State<ContactCard> {
                 Navigator.pushNamed(context, Home.id);
                 Fluttertoast.showToast(msg: "Update success");
               }).catchError((err) => print(err));
-
             },
             child: new Text('OK'),
           ),
@@ -189,12 +190,20 @@ class _ContactCardState extends State<ContactCard> {
 
   // Opens calling screen
   void _handleCall() async {
-    _launched = _makePhoneCallAndTextMessage('tel:$swaperPhoneNumber');
+    _launched = _evaluateUrl('tel:$swaperPhoneNumber');
   }
 
   // Opens messaging screen
   void _handleMessage() async {
-    _launched = _makePhoneCallAndTextMessage('sms:$swaperPhoneNumber');
+    _launched = _evaluateUrl('sms:$swaperPhoneNumber');
+  }
+
+  // Opens messaging screen
+  void _handleNavigation() async {
+    print("this is swaplocation: ${widget.swapLocation}");
+
+    print("this is urlLocation: $urlLocation");
+    _launched = _evaluateUrl(urlLocation);
   }
 
   Widget _launchStatus(BuildContext context, AsyncSnapshot<void> snapshot) {
@@ -205,11 +214,23 @@ class _ContactCardState extends State<ContactCard> {
     }
   }
 
-  Future<void> _makePhoneCallAndTextMessage(String url) async {
+  Future<void> _evaluateUrl(String url) async {
     if (await canLaunch(url)) {
       await launch(url);
     } else {
       throw 'Could not launch $url';
+    }
+  }
+
+  void getUrlLocation() async {
+    final locations =
+        await Firestore.instance.collection('parking_locations').getDocuments();
+    for (var location in locations.documents) {
+      if (location.data['name'] == widget.swapLocation) {
+        setState(() {
+          urlLocation = location.data['url'];
+        });
+      }
     }
   }
 }
